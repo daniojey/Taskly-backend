f36042e641bdeeb3ab6a19dfdf4bb99e447cfb7b
Saymios <daniojenezis@gmail.com>
2025-08-22 10:20:11 +0300

    Initialize project

---
+++ Django/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+app_name = "api"
+
+# urlpatterns = [
+#     path('/', admin.site.urls),
+# ]
+
+urlpatterns = [
+    # Примеры путей:
+]
---
+++ Django/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+app_name = "users"
+
+# urlpatterns = [
+#     path('/', admin.site.urls),
+# ]
+
+urlpatterns = [
+    # Примеры путей:
+]
