dfd693aca430da2cd4398fb8a7300cc41e6174b3
Saymios <daniojenezis@gmail.com>
2026-08-16 04:04:49 +0200

    changes: - update settings.py

--- Django/main/settings.py
+++ Django/main/settings.py
@@ -241,7 +241,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://localhost:4173',
-    'https://resplendent-sprinkles-4bec00.netlify.app'
+    'https://tasklyfront.netlify.app/'
 ]
 
 CORS_ALLOWED_ORIGINS = [
