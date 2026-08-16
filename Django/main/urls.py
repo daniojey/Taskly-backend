011651b639414e1a3d5f3dcc45e33c83c960146a
Saymios <daniojenezis@gmail.com>
2026-08-16 15:24:18 +0200

    changes: - update urls.py

--- Django/main/urls.py
+++ Django/main/urls.py
@@ -1,22 +1,7 @@
-"""
-URL configuration for main project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from main import settings
 import api
@@ -26,7 +11,8 @@ urlpatterns = [
 
     re_path(r'^api/(?P<version>(v1|v2))/', include("api.urls", namespace='api')),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
-    path('tasks/', include('task.urls', namespace='task'))
+    path('tasks/', include('task.urls', namespace='task')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 
