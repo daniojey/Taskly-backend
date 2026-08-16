b3fc5d2a3c20514c98ed3cb6f43513e1fe667e39
Saymios <daniojenezis@gmail.com>
2026-08-16 02:57:13 +0200

    changes: - update cors_allowed_origins

--- Django/main/settings.py
+++ Django/main/settings.py
@@ -249,7 +249,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # URL твоего React-приложения
     'http://localhost:5173', 
     'http://localhost:4173',
-    'https://resplendent-sprinkles-4bec00.netlify.app'
+    'https://tasklyfront.netlify.app/'
 ]
 
 CORS_ALLOW_CREDENTIALS = True  # Разрешить передачу кук
