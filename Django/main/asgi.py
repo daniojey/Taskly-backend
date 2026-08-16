0251be5997f6f178e589ef85abbc8b16616f8a3b
Saymios <daniojenezis@gmail.com>
2026-08-16 03:55:39 +0200

    changes: - update asgi.py

--- Django/main/asgi.py
+++ Django/main/asgi.py
@@ -8,12 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 django_asgi_app = get_asgi_application()
 
-from django.core.asgi import get_asgi_application
 from users.middleware import JWTAuthMIddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
